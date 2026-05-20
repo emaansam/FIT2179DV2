88e7b707d21c5d4d8849fd289d4cf5fa21db9735
emaansam <emaanasamdani@gmail.com>
2026-05-20 15:04:35 +1000

    Add files via upload

---
+++ dv2fidioms.js
@@ -0,0 +1,7 @@
+var vg_1 = "fortyplus.vl.json";
+vegaEmbed("#dumbell_chart", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "rankings.json";
+vegaEmbed("#rankings", vg_2).then(function(result) {
+}).catch(console.error);
